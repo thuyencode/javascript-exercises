fca4c3c8520acf99602a4ad8a81ac1f1bf7c546e
thuyencode <thuyencode@proton.me>
2023-11-13 23:37:38 +0700

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return 'ERROR'
+  }
 
-};
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR'
+  }
+
+  let result = num1 > num2 ? num2 : num1
+  const n = num1 > num2 ? num1 : num2
+
+  for (let i = 2; i <= n; i++) {
+    result += i
+  }
+
+  return result
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
