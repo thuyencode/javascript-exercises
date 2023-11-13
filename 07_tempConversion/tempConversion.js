c3187167ca763bad36f568c766a3412a843164bf
thuyencode <thuyencode@proton.me>
2023-11-13 23:57:28 +0700

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (fahrenheit = 32) {
+  if (typeof fahrenheit !== 'number') return undefined
 
-const convertToFahrenheit = function() {
-};
+  return parseFloat(((fahrenheit - 32) * (5 / 9)).toFixed(1))
+}
+
+const convertToFahrenheit = function (celsius = 0) {
+  if (typeof celsius !== 'number') return undefined
+
+  return parseFloat((celsius * (9 / 5) + 32).toFixed(1))
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
-};
+  convertToFahrenheit,
+}
