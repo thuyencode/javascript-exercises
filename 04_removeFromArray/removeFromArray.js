d8032134f570a7c5418f874a9578634ed5f9727b
thuyencode <thuyencode@proton.me>
2023-11-13 23:27:24 +0700

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr = [], ...targets) {
+  let newArr = []
 
-};
+  for (let i = 0; i < arr.length; i++) {
+    let canBeAdded = true
+
+    for (let j = 0; j < targets.length; j++) {
+      if (arr[i] === targets[j]) {
+        canBeAdded = false
+        break
+      }
+    }
+
+    if (canBeAdded) newArr.push(arr[i])
+  }
+
+  return newArr
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
