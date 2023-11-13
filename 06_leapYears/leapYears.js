f0b061de7a9af4dcfec54d640f4c74caa5de55a2
thuyencode <thuyencode@proton.me>
2023-11-13 23:44:35 +0700

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (!Number.isInteger(year)) return undefined
 
-};
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
