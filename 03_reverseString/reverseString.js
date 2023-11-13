b5707a2720bbf7aa76f223fb7eb80f87ac80c544
thuyencode <thuyencode@proton.me>
2023-11-13 23:14:55 +0700

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  if (typeof string !== 'string') return undefined
 
-};
+  let reversedString = ''
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string.charAt(i)
+  }
+
+  return reversedString
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
