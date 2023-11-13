3d1e005cb08785dbd9f9cd420b9c7b74ffbd950e
thuyencode <thuyencode@proton.me>
2023-11-13 21:01:53 +0700

    Complete the test 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (typeof string !== 'string') return 'ERROR'
+  if (typeof num !== 'number' || num < 0) return 'ERROR'
 
-};
+  if (string.length === 0) return ''
+  if (num === 0) return ''
+
+  return string.repeat(num)
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
