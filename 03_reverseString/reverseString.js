1e78df6053fe348978529e8646155240a01fafba
thuyencode <thuyencode@proton.me>
2023-11-14 09:16:36 +0700

    Move to more declarative solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,19 @@
+// const reverseString = function (string) {
+//   if (typeof string !== 'string') return undefined
+
+//   let reversedString = ''
+
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reversedString += string.charAt(i)
+//   }
+
+//   return reversedString
+// }
+
 const reverseString = function (string) {
   if (typeof string !== 'string') return undefined
 
-  let reversedString = ''
-
-  for (let i = string.length - 1; i >= 0; i--) {
-    reversedString += string.charAt(i)
-  }
-
-  return reversedString
+  return string.split('').reverse().join('')
 }
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,24 @@
+// const removeFromArray = function (arr = [], ...targets) {
+//   let newArr = []
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let canBeAdded = true
+
+//     for (let j = 0; j < targets.length; j++) {
+//       if (arr[i] === targets[j]) {
+//         canBeAdded = false
+//         break
+//       }
+//     }
+
+//     if (canBeAdded) newArr.push(arr[i])
+//   }
+
+//   return newArr
+// }
+
 const removeFromArray = function (arr = [], ...targets) {
-  let newArr = []
-
-  for (let i = 0; i < arr.length; i++) {
-    let canBeAdded = true
-
-    for (let j = 0; j < targets.length; j++) {
-      if (arr[i] === targets[j]) {
-        canBeAdded = false
-        break
-      }
-    }
-
-    if (canBeAdded) newArr.push(arr[i])
-  }
-
-  return newArr
+  return arr.filter((element) => !targets.includes(element))
 }
 
 // Do not edit below this line
