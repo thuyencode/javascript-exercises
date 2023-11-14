2c50e00b1386d4b7a69b42659d7d094aa67a4b17
thuyencode <thuyencode@proton.me>
2023-11-14 09:12:42 +0700

    Remove the unnecessary codes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,6 @@ const repeatString = function (string, num) {
   if (typeof string !== 'string') return 'ERROR'
   if (typeof num !== 'number' || num < 0) return 'ERROR'
 
-  if (string.length === 0) return ''
-  if (num === 0) return ''
-
   let result = ''
 
   for (let i = 0; i < num; i++) {
