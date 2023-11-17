7a7344ce907c4a1dec51b1e083a41be05bdec57a
thuyencode <thuyencode@proton.me>
2023-11-17 22:01:08 +0700

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (originalStr) {
+  if (typeof originalStr !== 'string') return undefined
 
-};
+  const str1 = originalStr.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+  const str2 = str1.split('').reverse().join('')
+
+  return str1 === str2
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
