810769571c0c9ff4b0ecbc321f8d531b433adde6
thuyencode <thuyencode@proton.me>
2023-11-17 22:07:24 +0700

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n === 0) return a
+  if (n < 0) return 'OOPS'
 
-};
+  let a = 0,
+    b = 1,
+    temp
+
+  for (let i = 2; i <= n; i++) {
+    temp = a
+    a = b
+    b = temp + b
+  }
+  return b
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
