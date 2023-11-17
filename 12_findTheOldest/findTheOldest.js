1d2d913e8abee419cb1d3475c41307e20968d08e
thuyencode <thuyencode@proton.me>
2023-11-17 22:51:14 +0700

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const getAge = function (birthYear, deathYear) {
+  if (!deathYear) {
+    deathYear = new Date().getFullYear()
+  }
 
-};
+  return deathYear - birthYear
+}
+
+const findTheOldest = function (
+  people = [{ name: '', yearOfBirth: 0, yearOfDeath: 0 }]
+) {
+  return people.sort(
+    (person1, person2) =>
+      getAge(person2.yearOfBirth, person2.yearOfDeath) -
+      getAge(person1.yearOfBirth, person1.yearOfDeath)
+  )[0]
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
